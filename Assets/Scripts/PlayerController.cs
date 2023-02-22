e803c71718a6f9be57548cd8a0cae42a675ce1e6
Letícia Oliveira <95142341+letceo@users.noreply.github.com>
2023-02-22 23:07:25 +0000

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    private bool isMoving;
+    private Vector2 input;
+
+    private void Update()
+    {
+        if(!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal"); //raw means the input will always be +1 or -1
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if(input != Vector2.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+
+            }
+
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving=true;
+        while((targetPos - transform.position).sqrMagnitude>Mathf.Epsilon) 
+        {
+            transform.position=Vector3.MoveTowards(transform.position, targetPos, moveSpeed*Time.deltaTime);
+            yield return null;
+        }
+        transform.position=targetPos;
+        isMoving=false;
+
+    
+
+
+    }
+
+}
